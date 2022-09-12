ba2fe0913bc3113008628d4383024f97adef2c9d
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2022-09-12 12:24:07 +0200

    Add files via upload

---
+++ inicijalnaProvjera_1.dio-5.zadatak/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inicijalnaProvjera_1.dio_5.zadatak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url;
+
+            Console.WriteLine("Upišite URL adresu:");
+            url = Console.ReadLine();
+
+            if (url.StartsWith("www.") && url.EndsWith(".hr"))
+            {
+                Console.WriteLine("URL je pravilan.");
+            }
+            else
+            {
+                Console.WriteLine("URL nije pravilan.");
+            }
+
+            Console.ReadKey();
+        }    
+    }
+}
